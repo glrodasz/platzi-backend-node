2b967bf6a807ef1c570e40836f940cdde11568b2
Guillermo Rodas <garethderioth@gmail.com>
2019-07-12 09:31:50 -0500

    Conexion a Mongo Atlas

---
+++ movies-api/lib/mongo.js
@@ -0,0 +1,34 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const { config } = require('../config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const DB_NAME = config.dbName;
+
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
+
+class MongoLib {
+  constructor() {
+    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+    this.dbName = DB_NAME;
+  }
+
+  connect() {
+    if (!MongoLib.connection) {
+      MongoLib.connection = new Promise((resolve, reject) => {
+        this.client.connect(err => {
+          if (err) {
+            reject(err);
+          }
+
+          console.log('Connected succesfully to mongo');
+          resolve(this.client.db(this.dbName));
+        });
+      });
+    }
+
+    return MongoLib.connection;
+  }
+}
+
+module.exports = MongoLib;
