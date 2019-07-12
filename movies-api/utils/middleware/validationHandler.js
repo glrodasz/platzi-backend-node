772311f573e9282d3904dc441d854dff3e76a64d
Guillermo Rodas <garethderioth@gmail.com>
2019-07-12 11:30:55 -0500

    Capa de validacion de datos usando un middleware

---
+++ movies-api/utils/middleware/validationHandler.js
@@ -0,0 +1,13 @@
+function validate() {
+  return false;
+}
+
+function validationHandler(schema, check = 'body') {
+  return function(req, res, next) {
+    const error = validate(req[check], schema);
+
+    error ? next(new Error(error)) : next();
+  };
+}
+
+module.exports = validationHandler;
