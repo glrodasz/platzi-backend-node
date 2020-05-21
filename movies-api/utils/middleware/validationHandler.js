56c5ad9f313c137b75d3126a7601cec7171e2d6f
Alexis Araujo <ala1289@gmail.com>
2020-05-21 16:13:38 -0500

    [FIX]Typo in validationHandler

--- movies-api/utils/middleware/validationHandler.js
+++ movies-api/utils/middleware/validationHandler.js
@@ -6,7 +6,7 @@ function validate(data, schema) {
   return error;
 }
 
-functio  validationHandler(schema, check = 'body') {
+function  validationHandler(schema, check = 'body') {
   return function(req, res, next) {
     const error = validate(req[check], schema);
 
