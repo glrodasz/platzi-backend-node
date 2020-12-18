1bec6eb658eef6785737e0d5f0d9a1842b8229cc
Guillermo Rodas <me@guillermorodas.com>
2020-12-18 16:31:08 +0100

    Remove .only

--- movies-api/test/utils.buildMessage.test.js
+++ movies-api/test/utils.buildMessage.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const buildMessage = require('../utils/buildMessage');
 
-describe.only('utils - buildMessage', function() {
+describe('utils - buildMessage', function() {
   describe('when receives na entity and an action', function() {
     it('should return the respective message', function() {
       const result = buildMessage('movie', 'create');
